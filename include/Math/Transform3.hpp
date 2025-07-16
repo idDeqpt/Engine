b6d16a1652d127f3dee7067af5355035fb6e2c43
Владислав Ушаков <ushakov_vl@list.ru>
2025-07-16 19:11:19 +0700

    Added Transform3 class

---
+++ include/Math/Transform3.hpp
@@ -0,0 +1,27 @@
+#ifndef TRANSFORM_3_CLASS_HEADER
+#define TRANSFORM_3_CLASS_HEADER
+
+#include "Mat4.hpp"
+#include "Vec3.hpp"
+
+namespace mth
+{
+	class Transform3
+	{
+	public:
+		Transform3();
+
+		void translate(const Vec3& vec);
+		void scale(const Vec3& vec);
+		void rotate(const Vec3& vec, float angle); //radians
+
+		Mat4 getMatrix();
+
+		static Mat4 getIdentity();
+
+	protected:
+		Mat4 matrix;
+	};
+}
+
+#endif //TRANSFORM_3_CLASS_HEADER
---
+++ src/Transform3.cpp
@@ -0,0 +1,80 @@
+#include <Math/Transform3.hpp>
+
+#include <cmath>
+
+#include <Math/Mat4.hpp>
+#include <Math/Vec3.hpp>
+
+
+mth::Transform3::Transform3()
+{
+	matrix = Transform3::getIdentity();
+}
+
+
+void mth::Transform3::translate(const Vec3& vec)
+{
+	Mat4 translate_m(
+		1, 0, 0, vec.x,
+		0, 1, 0, vec.y,
+		0, 0, 1, vec.z,
+		0, 0, 0, 1
+	);
+	matrix = matrix*translate_m;
+}
+
+void mth::Transform3::scale(const Vec3& vec)
+{
+	Mat4 scale_m(
+		vec.x, 0, 0, 0,
+		0, vec.y, 0, 0,
+		0, 0, vec.z, 0,
+		0, 0, 0, 1
+	);
+	matrix = matrix*scale_m;
+}
+
+void mth::Transform3::rotate(const Vec3& vec, float angle)
+{
+	float a = angle;
+	float s = sin(a);
+	float c = cos(a);
+
+	Vec3 axis(vec.norm(1));
+	Vec3 temp((1 - c)*axis);
+
+	Mat4 rotate_m;
+	rotate_m[0][0] = c + temp[0]*axis[0];
+	rotate_m[0][1] = temp[0]*axis[1] + s*axis[2];
+	rotate_m[0][2] = temp[0]*axis[2] - s*axis[1];
+
+	rotate_m[1][0] = temp[1]*axis[0] - s*axis[2];
+	rotate_m[1][1] = c + temp[1]*axis[1];
+	rotate_m[1][2] = temp[1]*axis[2] + s*axis[0];
+
+	rotate_m[2][0] = temp[2]*axis[0] + s*axis[1];
+	rotate_m[2][1] = temp[2]*axis[1] - s*axis[0];
+	rotate_m[2][2] = c + temp[2]*axis[2];
+
+	rotate_m[3][3] = 1;
+
+	matrix = matrix*rotate_m;
+}
+
+
+mth::Mat4 mth::Transform3::getMatrix()
+{
+	return matrix;
+}
+
+
+
+mth::Mat4 mth::Transform3::getIdentity()
+{
+	return Mat4(
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	);
+}
