a9a32d740823dc41799020b0324f27ab83db2033
Владислав Ушаков <ushakov_vl@list.ru>
2025-07-21 21:32:39 +0700

    Fix typo

--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -11,7 +11,7 @@ mth::Vec2::Vec2(float v1, float v2) :
 	x(v1), y(v2) {}
 
 mth::Vec2::Vec2(const Vec2& vec):
-	x(vec.x), y(vec.y), z(vec.z) {}
+	x(vec.x), y(vec.y) {}
 
 
 mth::Vec2& mth::Vec2::operator=(const Vec2& vec)
