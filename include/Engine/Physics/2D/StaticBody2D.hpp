d9a17ec9f6845bc4c023e27693b9116dbefc7161
idDeqpt <ushakov_vl@list.ru>
2026-04-27 16:53:38 +0700

    ref: add .cpp for StaticBody2D

--- include/Engine/Physics/2D/StaticBody2D.hpp
+++ include/Engine/Physics/2D/StaticBody2D.hpp
@@ -2,28 +2,26 @@
 #define STATIC_BODY_2D_CLASS_HEADER
 
 #include <Engine/Physics/2D/PhysicsBody2D.hpp>
-#include <Engine/Physics/2D/CollisionData.hpp>
-#include <Engine/Math/Vec2.hpp>
-
 
 namespace eng::phy
 {
 	class RigidBody2D;
+	class CollisionData;
 
 	class StaticBody2D : public PhysicsBody2D
 	{
 	public:
-		StaticBody2D() : phy::PhysicsBody2D() {}
+		StaticBody2D();
 
-		void updateState(float delta) override {};
+		void updateState(float delta);
 
-		void resolveCollisionVelWith(const CollisionData& data, PhysicsBody2D& other) override {other.resolveCollisionVelWithStatic(data, *this);};
-		void resolveCollisionVelWithRigid(const CollisionData& data, RigidBody2D& other) override {other.resolveCollisionVelWithStatic(data, *this);};
-		void resolveCollisionVelWithStatic(const CollisionData& data, StaticBody2D& other) override {};
+		void resolveCollisionVelWith(const CollisionData& data, PhysicsBody2D& other);
+		void resolveCollisionVelWithRigid(const CollisionData& data, RigidBody2D& other);
+		void resolveCollisionVelWithStatic(const CollisionData& data, StaticBody2D& other);
 
-		void resolveCollisionPosWith(const CollisionData& data, float iter_ratio, PhysicsBody2D& other) override {other.resolveCollisionPosWithStatic(data, iter_ratio, *this);};
-		void resolveCollisionPosWithRigid(const CollisionData& data, float iter_ratio, RigidBody2D& other) override {other.resolveCollisionPosWithStatic(data, iter_ratio, *this);};
-		void resolveCollisionPosWithStatic(const CollisionData& data, float iter_ratio, StaticBody2D& other) override {};
+		void resolveCollisionPosWith(const CollisionData& data, float iter_ratio, PhysicsBody2D& other);
+		void resolveCollisionPosWithRigid(const CollisionData& data, float iter_ratio, RigidBody2D& other);
+		void resolveCollisionPosWithStatic(const CollisionData& data, float iter_ratio, StaticBody2D& other);
 	};
 }
 
---
+++ src/Physics/2D/StaticBody2D.cpp
@@ -0,0 +1,50 @@
+#include <Engine/Physics/2D/StaticBody2D.hpp>
+
+#include <Engine/Physics/2D/PhysicsBody2D.hpp>
+#include <Engine/Physics/2D/RigidBody2D.hpp>
+#include <Engine/Physics/2D/CollisionData.hpp>
+
+
+namespace eng
+{
+
+phy::StaticBody2D::StaticBody2D() : phy::PhysicsBody2D() {}
+
+
+void phy::StaticBody2D::updateState(float delta)
+{
+
+}
+
+
+void phy::StaticBody2D::resolveCollisionVelWith(const CollisionData& data, PhysicsBody2D& other)
+{
+	other.resolveCollisionVelWithStatic(data, *this);
+}
+
+void phy::StaticBody2D::resolveCollisionVelWithRigid(const CollisionData& data, RigidBody2D& other)
+{
+	other.resolveCollisionVelWithStatic(data, *this);
+}
+
+void phy::StaticBody2D::resolveCollisionVelWithStatic(const CollisionData& data, StaticBody2D& other)
+{
+
+}
+
+void phy::StaticBody2D::resolveCollisionPosWith(const CollisionData& data, float iter_ratio, PhysicsBody2D& other)
+{
+	other.resolveCollisionPosWithStatic(data, iter_ratio, *this);
+}
+
+void phy::StaticBody2D::resolveCollisionPosWithRigid(const CollisionData& data, float iter_ratio, RigidBody2D& other)
+{
+	other.resolveCollisionPosWithStatic(data, iter_ratio, *this);
+}
+
+void phy::StaticBody2D::resolveCollisionPosWithStatic(const CollisionData& data, float iter_ratio, StaticBody2D& other)
+{
+
+}
+
+} //namespace eng
